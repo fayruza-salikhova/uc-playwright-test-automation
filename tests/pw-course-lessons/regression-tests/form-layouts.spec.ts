29840a20b9f704e3bebaa13fdfdd100d7eeb4eb4
Fayruza Salikhova <biruzka@gmail.com>
2026-07-02 13:20:20 +0200

    faker package

--- tests/pw-course-lessons/regression-tests/form-layouts.spec.ts
+++ tests/pw-course-lessons/regression-tests/form-layouts.spec.ts
@@ -1,4 +1,5 @@
 import { test } from '../fixtures/test.fixtures';
+import { faker } from '@faker-js/faker';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:4200/');
@@ -7,10 +8,16 @@ test.beforeEach(async ({ page }) => {
 test('form submittings', async ({ navigationPage, formLayoutsPage }) => {
   await navigationPage.goFormLayoutsPage();
 
+  const randomFullName = faker.person.fullName({ firstName: 'TestPerson' });
+  const randomEmail = `${randomFullName.replace(/\s/g, '')}${faker.number.int({ max: 1000 })}@test.com`;
   await formLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOptions(
     'tesdt@test.com',
     'Welcome1',
     'Option 2',
   );
-  await formLayoutsPage.submitInlineFormWithNameEmailAndCheckbox('Smith', 'smith@test.com', false);
+  await formLayoutsPage.submitInlineFormWithNameEmailAndCheckbox(
+    randomFullName,
+    randomEmail,
+    false,
+  );
 });
